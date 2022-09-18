a1712355f6b20f4aa6f0c52bdb7b7f26c36661f2
Jaineel3105 <jaineel.jp@gmail.com>
2022-09-18 19:34:59 +0530

    Practical-3 completed

--- app/src/main/java/com/example/madpractical3_20012021027/LoginActivity.kt
+++ app/src/main/java/com/example/madpractical3_20012021027/LoginActivity.kt
@@ -1,21 +1,32 @@
 package com.example.madpractical3_20012021027
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
 import androidx.core.view.WindowCompat
 import com.example.madpractical3_20012021027.databinding.ActivityLoginBinding
 import com.example.madpractical3_20012021027.databinding.ActivityMainBinding
 
 class LoginActivity : AppCompatActivity() {
-    private  lateinit var binding: ActivityLoginBinding
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
-        WindowCompat.setDecorFitsSystemWindows(window,false)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
 
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
+
+        binding.BottomNavigationView.id = R.id.bottom_nav_reg
+        binding.BottomNavigationView.setOnItemSelectedListener { it2 ->
+            when (it2.itemId) {
+                R.id.bottom_nav_reg -> {
+                    Intent(this, RegistrationActivity::class.java).also { startActivity(it) }
+                }
+            }
+            return@setOnItemSelectedListener true
+        }
     }
 }
+
--- app/src/main/java/com/example/madpractical3_20012021027/RegistrationActivity.kt
+++ app/src/main/java/com/example/madpractical3_20012021027/RegistrationActivity.kt
@@ -3,18 +3,18 @@ package com.example.madpractical3_20012021027
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.view.WindowCompat
-import com.example.madpractical3_20012021027.databinding.ActivityLoginBinding
+import com.example.madpractical3_20012021027.databinding.ActivityMainBinding
 import com.example.madpractical3_20012021027.databinding.ActivityRegistrationBinding
 
 class RegistrationActivity : AppCompatActivity() {
-    private  lateinit var binding: ActivityRegistrationBinding
+    private lateinit var binding: ActivityRegistrationBinding
     override fun onCreate(savedInstanceState: Bundle?) {
-        WindowCompat.setDecorFitsSystemWindows(window,false)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
-
         binding = ActivityRegistrationBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
+
     }
 }
